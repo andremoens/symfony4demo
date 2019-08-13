4a858b612189e80a9de41f20e1642fcd420c1e12
Andre Moens <andre@3menc.nl>
2019-08-13 17:19:29 +0200

    Some initial twig things

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,8 +4,9 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
 
     /**
@@ -14,5 +15,23 @@ class ArticleController
 		public function homepage()
     {
     	return new Response('OMG! My first page already! WOOO!');
+    }
+    
+   /**
+   * @Route("/news/{slug}")
+   */
+    public function show($slug)
+    {
+    
+			$comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];    
+    
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
     }
 }
