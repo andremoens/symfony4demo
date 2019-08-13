5fd249fac410dd389751444706e1c716fe356db9
Andre Moens <andre@3menc.nl>
2019-08-13 16:31:58 +0200

    Tnstalled twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class ArticleController
 {
 
+    /**
+     * @Route("/")
+     */
 		public function homepage()
     {
     	return new Response('OMG! My first page already! WOOO!');
